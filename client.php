c06efb0042d2f459b9e8997f9f54da03c2e4a7ac
Fatoudrame <77timbs@gmail.com>
2023-06-24 14:18:55 +0000

    creation des fichiers html

--- client.php
+++ client.php
@@ -3,10 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS\style.css">
     <title>Document</title>
 </head>
 <body>
     <h1>Ajouter client</h1>
-    
+   <div class="cadre">
+    <form action="" method="post">
+         
+        <input type="text" name="nom" id="nom" placeholder="NOM">
+        <input type="text" name="firstname" id="firstname" placeholder="Prénom">
+       <div class="flex">
+        <select id="sexe" name="sexe">
+            <option value="homme">Homme</option>
+            <option value="femme">Femme</option>
+            </select></div>
+        <input type="number" name="age">
+        <input type="submit" name="envoyer" id="envoyer">
+    </form>
+      </div>
 </body>
 </html>
--- maison.php
+++ maison.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS\style.css">
     <title>Document</title>
 </head>
 <body>
     <h1>Ajouter maison</h1>
+    <div class="cadre">
+    <form action="" method="post">
+         
+        <input type="text" name="quartier" id="quartier" placeholder="Quartier">
+        <input type="text" name="ville" id="ville" placeholder="Ville">
+        <input type="number" name="proprietaire" placeholder="id_proprietaire">
+        <input type="submit" name="envoyer" id="envoyer">
+    </form>
+      </div>
 </body>
 </html>
