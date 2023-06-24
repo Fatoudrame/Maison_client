c4f9459c6e3887d9dd888615645c6b638928e662
Fatoudrame <77timbs@gmail.com>
2023-06-24 12:35:23 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Page d'inscription</h1>
+    <div class="button">
+        <a href="maison.php">Ajouter maison</a>
+        <a href="client.php">Ajouter client</a>
+    </div>
+</body>
+</html>
